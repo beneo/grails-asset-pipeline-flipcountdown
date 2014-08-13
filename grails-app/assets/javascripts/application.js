c9e570852874e0cba3d3ed4cee83db69d2949af3
wuzu.wth <wuzu.wth@taobao.com>
2014-08-13 17:24:11 +0800

    i put the original html source code in the folder source_code, i want to refactor html to gsp, and use asset-pipeline to manage css/js, but i failed. can you help me and show me the best practice of using asset-pipeline. there are just two pages need refactor, dashboard.gsp and the alerts_btns.gsp, there are different js and css files, i think one application.js and one application.css will be huge if i put all together.

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -1,6 +1,19 @@
 //= encoding UTF-8
 //= require_tree .
 //= require_tree ../flipcountdown
+//= require_tree ../bootstrap
+//= require_tree ../lib/jquery-ui
+//= require_tree ../lib/typeahead
+//= require_tree ../lib/google-code-prettify
+//= require_tree ../lib/sticky
+//= require_tree ../lib/qtip2
+//= require_tree ../lib/colorbox
+//= require_tree ../lib/jBreadcrumbs
+//= require_tree ../lib/slimScroll
+//= require_tree ../lib/UItoTop
+//= require_tree ../lib/flot
+//= require_tree ../lib/fullcalendar
+//= require_tree ../lib/list_js
 //= require_self
 
 jQuery(function($){
